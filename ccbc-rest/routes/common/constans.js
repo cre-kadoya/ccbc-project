717a8e02098acc15a60b018951d16244a49c37ee
cre-kadoya <kadoya.tak@gmail.com>
2019-11-29 11:00:45 +0900

    環境変数名を変更

--- ccbc-rest/routes/common/constans.js
+++ ccbc-rest/routes/common/constans.js
@@ -1,4 +1,4 @@
-exports.bcdomain = process.env.BC_DOMAIN
-exports.jimuAccount = process.env.JIMU_ACCOUNT
-exports.jimuPassword = process.env.JIMU_PASSWORD
-exports.jimuShainPk = process.env.JIMU_SHAIN_PK
+exports.bcdomain = process.env.CCBC_BC_DOMAIN
+exports.jimuAccount = process.env.CCBC_JIMU_ACCOUNT
+exports.jimuPassword = process.env.CCBC_JIMU_PASSWORD
+exports.jimuShainPk = process.env.CCBC_JIMU_SHAIN_PK
--- ccbc-rest/routes/common/sequelize_helper.js
+++ ccbc-rest/routes/common/sequelize_helper.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 
 exports.sequelize = new Sequelize(
-  process.env.DATABASE_URL_HARVEST,
+  process.env.CCBC_DATABASE_URL_HARVEST,
   {
     dialect: 'postgres',
     operatorsAliases: false,
@@ -10,7 +10,7 @@ exports.sequelize = new Sequelize(
 )
 
 exports.sequelize2 = new Sequelize(
-  process.env.DATABASE_URL_HARVEST_GROUP,
+  process.env.CCBC_DATABASE_URL_HARVEST_GROUP,
   {
     dialect: 'postgres',
     operatorsAliases: false,
@@ -19,9 +19,9 @@ exports.sequelize2 = new Sequelize(
 )
 
 exports.sequelize3 = function(db_name) {
-  console.log('DBInfo:' + process.env.DATABASE_URL + db_name)
+  console.log('DBInfo:' + process.env.CCBC_DATABASE_URL + db_name)
   return new Sequelize(
-    process.env.DATABASE_URL + db_name,
+    process.env.CCBC_DATABASE_URL + db_name,
     {
       dialect: 'postgres',
       operatorsAliases: false,
